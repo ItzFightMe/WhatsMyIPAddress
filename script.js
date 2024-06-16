7868c90972c9c7532dfa0d1290803679c360291a
FekMi <103499204+ItzFightMe@users.noreply.github.com>
2024-06-17 03:37:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('check-ip-btn').addEventListener('click', function() {
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('ip-address').innerText = `Your IP Address is: ${data.ip}`;
+        })
+        .catch(error => {
+            console.error('Error fetching the IP address:', error);
+            document.getElementById('ip-address').innerText = 'Could not fetch the IP address. Please try again later.';
+        });
+});
